78338f454a40e4bae17b80a7f2f16f2967559f0d
Yuliya110692 <122671093+Yuliya110692@users.noreply.github.com>
2023-07-22 16:21:21 +0100

    adding new routes

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const express = require ("express")
 const app = express()
 
 
-app.get("/", (req, res) => {
-    res.send({ hello: "class10"});
+app.get("/", (request, response) => {
+    response.send({ hello: "class10"});
 });
+
+app.get("/order", (request, response)=> {
+  response.send("Hi")
+})
+
+app.get("/order/counter", (request, response)=> {
+  response.send(("Pic"))
+})
 
 
 app.listen(3000,()=> {
     console.log ("my app is listennig on port 3000")
 })
+
